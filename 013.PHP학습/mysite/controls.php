b5664078316a63a5833a8d536abad8ac3d6c49b9
Tom <tombap8@hotmail.com>
2021-02-15 16:09:00 +0900

    Update controls.php

--- 013.PHP학습/mysite/controls.php
+++ 013.PHP학습/mysite/controls.php
@@ -1,33 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>PHP 제어문 연습</title>
     <style>
-        img{
+        img {
             width: 50px;
         }
+
     </style>
     <script>
-    //버튼을 클릭하면 입력된 숫자를 가지고 Get방식으로
-    // 자기자신 페이지 다시 호출하기~!
-        window.addEventListener("load",function(){
-            
+        //버튼을 클릭하면 입력된 숫자를 가지고 Get방식으로
+        // 자기자신 페이지 다시 호출하기~!
+        window.addEventListener("load", function() {
+
             document.querySelector('.btn')
-            .onclick = function(){
-                
-                location.href = "controls.php?num="+
-                document.querySelector('.txt').value;
-                
-            };////// onclick //////////////
-            
-            
-        });/////////// load //////////////////////
-    
+                .onclick = function() {
+
+                    location.href = "controls.php?num=" +
+                        document.querySelector('.txt').value;
+
+                }; ////// onclick //////////////
+
+
+        }); /////////// load //////////////////////
+
     </script>
 </head>
+
 <body>
-  <?php
+    <?php
     
     // 일반적인 배열
     $tit = [
@@ -38,6 +41,63 @@
     
     // 배열순번랜덤수
     $rdn = rand(0,2);
+    /*
+    [연관배열(associative array)]
+    
+    JS 의 객체와 비슷하지만 이것은 단지
+    배열에 이름을 붙여서 쓸 수 있게 한것임!
+        
+        변수 = [
+            별칭 => 값,
+            별칭 => 값, ...
+        ];
+    
+    연관배열의 호출은
+        
+        변수[별칭]
+    */    
+        
+    $isrc = [
+        "마을"=>"images/01.png",
+        "별밤"=>"images/02.png",
+        "카페"=>"images/03.png",
+        "바다"=>"images/04.png"
+    ];
+    
+    // 일반적인 for문 사용하기
+    //for(시작값;한계값;증감){코드}
+    /*
+    for문 내부에서 사용하는 2가지 키워드
+    1) continue 를 만나면 아래 코드 실행안하고 위로 
+        올라가서 for문을 계속 돈다!
+    2) break 를 만나면 for문을 빠져 나간다!
+    -> 보통 조건문if와 같이 사용된다!
+    */
+    // 내가 좋아하는 숫자 찍기
+    
+    //내가 좋아하는 숫자 변수
+    $mynum = "";
+    
+    // 여기서 3,7,8번을 찍으려고 함!
+    for($x = 0 ; $x <= 9 ; $x++){
+        
+        if($x==0 || $x==1 || $x==2 || $x==4 ||
+           $x==5 || $x==6 || $x==9 ){
+            continue; // 해당번호 아래코드로 안감!
+        }////// if ///////////////////////////
+        
+        $mynum .= $x;
+        // .= 은 문자대입연산자
+        
+        if($x==8) break; //여기서 for문 나감!
+        
+        $mynum .= ",";
+    } ////// for 문 /////////
+    
+   
+    
+    
+    
     
     
     // 이미지 출력용변수
@@ -54,28 +114,44 @@
     if(isset($_GET["num"])){
         // 변수에 넘어온 숫자값 넣기
         $txt = $_GET["num"];
-        echo $txt;
+        //echo $txt;
+        
+        // 넘어온 문자형 숫자를 잘라서 배열에 넣기!
+        $txt = str_split($txt);
+        print_r($txt);
+        
+        //str_split() 메서드 사용!
+        //-> 문자를 한글자씩 잘라서 배열에 넣어준다
+        
+        //print_r() 메서드
+        //-> 배열에 담긴 값을 확인하는 방법
         
     } ///// if //////////////////
+    
+
     
     
     
     
     
     ?>
-  
-   <h1>PHP 제어문 연습</h1>
-   <h2>입력된 숫자를 이미지로 출력하기</h2>
-   
-   <h2><?=$tit[$rdn]?></h2>
-   <figure>
-       <img src="<?= ?>" alt="오늘의 이미지">
-   </figure>
-   
-   <input type="text" class="txt">
-   <button class="btn">이미지로 변환하기</button>
-   <br>
-   <?=$prt?>
-    
+
+    <h1>PHP 제어문 연습</h1>
+    <h2>입력된 숫자를 이미지로 출력하기</h2>
+
+    <h2><?=$tit[$rdn]?></h2>
+    <figure>
+        <img src="<?= $isrc["마을"] ?>" alt="오늘의 이미지" style="width:auto;">
+    </figure>
+
+    <h2>0~9중에 내가 좋아하는 숫자는 <?=$mynum?>다!</h2>
+
+   <h2>숫자를 쓰고 변환버튼을 누르면 이미지로 바뀐다!</h2>
+    <input type="text" class="txt">
+    <button class="btn">이미지로 변환하기</button>
+    <br>
+    <?=$prt?>
+
 </body>
+
 </html>
