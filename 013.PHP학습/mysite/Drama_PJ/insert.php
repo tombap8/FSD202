c08dd62809d91704cd21e642d5cea9b05773d27a
Tom <tombap8@hotmail.com>
2021-02-25 15:10:40 +0900

    ss

---
+++ 013.PHP학습/mysite/Drama_PJ/insert.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>드라마 데이터 입력하기</title>
+    <style>
+        body{
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+   <h1>드라마 데이터 입력하기</h1>
+   
+   
+   
+   
+    
+</body>
+</html>
