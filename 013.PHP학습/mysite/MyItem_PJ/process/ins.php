54df025f031be81b643efe712099279e435cdd00
Tom <tombap8@hotmail.com>
2021-03-03 15:37:18 +0900

    Create ins.php

---
+++ 013.PHP학습/mysite/MyItem_PJ/process/ins.php
@@ -0,0 +1,22 @@
+<?php
+########## 회원가입 입력처리 페이지 ############
+# member.php 페이지에서 POST방식으로 넘어온 데이터 받기
+
+// 1.아이디
+$mid = $_POST["mid"];
+// 2.비번
+$mpw = $_POST["mpw"];
+// 3.이름
+$mnm = $_POST["mnm"];
+// 4.성별
+$gen = $_POST["gen"];
+// 5-1.이메일 앞주소
+$email1 = $_POST["email1"];
+// 5-2.이메일 뒷주소
+$seleml = $_POST["seleml"];
+// 5-3.직접입력 이메일 뒷주소
+$email2 = $_POST["email2"];
+
+
+
+?>
