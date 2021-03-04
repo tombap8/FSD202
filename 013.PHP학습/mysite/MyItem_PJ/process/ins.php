29a37b461c156ec891eece58881076f30803a8a2
Tom <tombap8@hotmail.com>
2021-03-04 16:26:01 +0900

    Update ins.php

--- 013.PHP학습/mysite/MyItem_PJ/process/ins.php
+++ 013.PHP학습/mysite/MyItem_PJ/process/ins.php
@@ -17,10 +17,19 @@ $seleml = $_POST["seleml"];
 // 5-3.직접입력 이메일 뒷주소
 $email2 = $_POST["email2"];
 
+/// 비밀번호는 암호화 하여 입력해야한다!!!
+// PHP의 암호와 방식 중 하나!
+// password_hash(일반문자비밀번호, PASSWORD_DEFAULT)
+// 같은 비번이라도 만들때 마다 다른문자로 생성된다!
+// 따라서 일반적인 복호화는 불가능하다!
+
+// 암호화된 비밀번호로 변환하여 다시 할당!
+$mpw = password_hash($mpw, PASSWORD_DEFAULT);
+
 //echo "1.아이디 : $mid<br>";
 //echo "2.비번 : $mpw<br>";
 //echo "3.이름 : $mnm<br>";
-echo "4.성별 : $gen<br>";
+//echo "4.성별 : $gen<br>";
 //echo "5-1.이멜앞 : $email1<br>";
 //echo "5-2.이멜뒷 : $seleml<br>";
 //echo "5-3.직접입력 : $email2<br>";
@@ -34,7 +43,16 @@ $seleml === "free" ?
 // 해석: 선택박스값이 "free"이면 이메일 직접입력값 넣고
 //      아니면 이메일 선택값을 넣어준다!
 
-echo $email1."@".$emailback;
+//echo $email1."@".$emailback;
+
+// INSERT 쿼리문 만들기
+$sql = "INSERT INTO `member`
+(`mid`, `mpw`, `name`, `gen`, `email1`, `email2`) 
+VALUES 
+('$mid','$mpw','$mnm','$gen','$email1','$emailback')";
+
+echo $sql;
+
 
 
 
