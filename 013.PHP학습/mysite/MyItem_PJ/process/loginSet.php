4c5551b9feb4cd88bc85bfd5075d4dd5cd18868a
Tom <tombap8@hotmail.com>
2021-03-08 16:45:25 +0900

    Update loginSet.php

--- 013.PHP학습/mysite/MyItem_PJ/process/loginSet.php
+++ 013.PHP학습/mysite/MyItem_PJ/process/loginSet.php
@@ -15,10 +15,49 @@ $mpw = $_POST["mpw"];
 # 해당 아이디로 DB에 쿼리하여 비밀번호를 가져온다!
 # 비밀번호를 입력한 것과 비교하여 검사 결과를 낸다!
 
+# 어떻게 비밀번호를 비교할 것인가???
+# DB에 입력된 비번은 해시암호와된 비번이므로
+# 단순 문자 비교를 하면 절대로 같을 수 없다!!!
+
+# php 에서 해시암호화 문자 비교 메서드가 있음!
+# password_verify(입력된비번, DB해시비번)
+# 결과: 만약 같으면 true, 같지않으면 false를 리턴해준다!
+
 // 쿼리문 만들기
 $sql = "SELECT `mid`,`mpw`,`name`,`auth` FROM `member` WHERE `mid` = '$mid'";
 
-echo $sql;
+//echo $sql;
+
+# DB연결하기
+include "dbcon.inc";
+
+# 쿼리 날리기
+# $conn->query(쿼리문)
+$res = $conn->query($sql);
+
+# 1. 레코드 유무 판별
+# $res->num_rows 결과레코드 개수를 담은 속성
+$cnt = $res->num_rows;
+
+echo "레코드개수: $cnt";
+
+# 2.레코드 개수가 1인 경우 비밀번호 비교하기
+if($cnt){
+    
+    # $res->fetch_assoc() : 
+    # 결과집합의 레코드를 이름으로 가져온다!
+    $row = $res->fetch_assoc();
+
+    echo $row["mpw"];
+    
+} ////////// if문 //////////////////////
+
+# 3.레코드가 없는 경우
+else{
+    
+    echo "레코드가 없습니다!";
+    
+} ////////// else문 /////////////////////
 
 
 
