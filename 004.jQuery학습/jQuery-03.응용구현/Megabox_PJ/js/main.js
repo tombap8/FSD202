a50e20bc6a467809f229f2c94c07b2ecdb431d73
Tom <tombap8@hotmail.com>
2021-03-11 13:17:09 +0900

    ss

--- 004.jQuery학습/jQuery-03.응용구현/Megabox_PJ/js/main.js
+++ 004.jQuery학습/jQuery-03.응용구현/Megabox_PJ/js/main.js
@@ -6,6 +6,8 @@ let autoI;
 let autoT;
 // 포스터상태 전역변수
 let ptsts = 1; //1-허용,0-불허용
+// 스와이퍼 전역변수
+let swiper;
 
 //////// 로드구역 ////////////////////////////////
 // addEventListener(이벤트명,함수) - JS내장함수
@@ -684,25 +686,25 @@ $(function () { /// jQB ///////////////////////
 
     }); //////// mouseenter ////////////////////
     ////////////////////////////////////////////
-    
+
     //// 스크린 축소/확대 기능 구현 ///////
     // 원리: 미리 셋팅된 확대 클래스를 넣었다 뺐다함 ///
     // 이벤트 대상: .expand a
     // 변경 대상: #screen
-    $(".expand a").click(function(e){
-        
+    $(".expand a").click(function (e) {
+
         //기본이동막기
         e.preventDefault();
-        
+
         //스크린에 클래스 "on" 넣기/빼기
         $("#screen").toggleClass("on");
         // toggleClass() 메서드는 
         // 클래스가 없으면 넣고 있으면 뺌!
-        
-    });//////// click /////////////////
-    
-    
-    
+
+    }); //////// click /////////////////
+
+
+
 
     ///////////////////////////////////////////
     /// GNB a요소 클릭시 스크롤 애니메이션 하기 ////
@@ -731,7 +733,9 @@ $(function () { /// jQB ///////////////////////
         // (범용선택요소: 즉, 여러브라우저에서 공통사용됨!)
         $("html,body").stop().animate({
             scrollTop: pgpos + "px"
-        }, 1200, "easeInOutQuint"); ///// animate /////
+        }, 1200, "easeInOutQuint", pageAction);
+        ///// animate /////
+        // 맨 끝에 콜백함수로 페이지액션함수를 호출한다!
         // stop()을 사용하여 여러개를 클릭했을때 마지막 선택
         // 만 남아서 처리되도록 중간에 쌓인 애니메이션 지움!
 
@@ -754,6 +758,32 @@ $(function () { /// jQB ///////////////////////
             .siblings().removeClass("on");
 
     }); ///////// click ///////////////////
+
+
+    ///// 3. 영화페이지 : 스와이퍼 적용하기 //////
+    // swiper변수를 전역변수로 만들고 페이지액션에서 사용!
+    swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1, //한 영역에 보여줄 슬라이드수
+        spaceBetween: 0, //슬라이드 사이간격
+        loop: true, //무한이동
+        pagination: { // 하단 블릿표시
+            el: '.swiper-pagination',
+            clickable: true, //블릿클릭이동
+        },
+        navigation: { // 양쪽이동버튼
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        // 자동넘김을 미리셋팅하면 본 페이지 오기전부터
+        // 이미 넘어가고 있다.... 그러므로 메서드를 사용해야함!
+//        autoplay: { // 자동넘김
+//            delay: 3000,//사이간격시간
+//            disableOnInteraction: false,
+//            //스와이프 상호작용후 다시자동넘김옵션
+//            //(false가 기본값-상호작용후 사이간격시간
+//            //있다가 다시 자동넘김작동함)
+//        },
+    }); //////// swiper ///////////////////////////
 
 
 
