e55f13d1a804533565bef4cc193e02f43ce73ac1
Tom <tombap8@hotmail.com>
2021-03-12 11:44:23 +0900

    ss

--- 004.jQuery학습/jQuery-03.응용구현/SliceMove_PJ/js/main.js
+++ 004.jQuery학습/jQuery-03.응용구현/SliceMove_PJ/js/main.js
@@ -1,31 +1,43 @@
 // 브라우저 다운로드 사이트 메인 JS - main.js 
 
-$(function(){ /// jQB //////////////////
-    
+$(function () { /// jQB //////////////////
+
     // 햄버거 버튼 클릭시 메뉴 들어오기
-    $("#ham").click(function(){
-        
+    $("#ham").click(function () {
+
         // 메뉴 들어오기
         $(".gnb").animate({
             left: "0"
-        }, 500,"easeInOutCubic");
-        
-        // 컨텐츠박스 오른쪽이동하기!
-        $(".wrap").animate({
-            left: "300px"
-        }, 500,"easeInOutCubic");
-        
-        
-        // 상단박스 오른쪽이동하기!
-        $(".top").animate({
-            left: "300px"
-        }, 500,"easeInOutCubic");
-        
-        
-        
-    });/////// click /////////////////
-    
-    
-    
-});////////// jQB //////////////////////
+        }, 500, "easeInOutCubic");
 
+        // 컨텐츠박스+상단박스(.stg) 오른쪽이동하기!
+        $(".stg").animate({
+            left: "300px"
+        }, 500, "easeInOutCubic");
+
+        // 메뉴 리스트 순서대로 들어오기
+        // 대상: .gnb li 
+        // 구현: li가 왼쪽에 나가있고 순서대로
+        //      애니메이션 하여 원위치함
+        //      이때 delay시간을 순서대로 증가시킴
+        // 사용메서드: each()
+        $(".gnb li").each(function (idx, ele) {
+            // idx-요소순번, ele-요소자신
+            //console.log(idx);
+            // 애니전 delay시간은 
+            // 0,100,200,300...순으로 셋팅됨!
+            $(ele).delay(100 * idx)
+                .animate({
+                    left: "0"
+                },400,"easeOutQuint");
+            ///////// animate /////////
+
+        }); //////// each ///////////////////
+
+
+
+    }); /////// click /////////////////
+
+
+
+}); ////////// jQB //////////////////////
