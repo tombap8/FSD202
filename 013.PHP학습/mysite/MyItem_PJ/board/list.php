e78e2ffa3f50e6e54647a2e14b0ac24484e3d827
Tom <tombap8@hotmail.com>
2021-03-22 16:09:35 +0900

    sss

---
+++ 013.PHP학습/mysite/MyItem_PJ/board/list.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>게시판 리스트 페이지</title>
+</head>
+<body>
+   
+   <!--게시판 리스트-->
+    <table class="dtbl">
+        <caption>방명록 게시판</caption>
+        <!--상단 컬럼명 표시영역-->
+        <thead>
+            <tr>
+                <th>번호</th>
+                <th>글 제목</th>
+                <th>글 쓴이</th>
+                <th>등록일자</th>
+                <th>조회수</th>
+            </tr>
+        </thead>
+        <!--하단 페이징 표시부분-->
+        <tfoot>
+            
+        </tfoot>
+        <!--중앙 레코드 표시부분-->
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+   
+   
+   
+    
+</body>
+</html>
