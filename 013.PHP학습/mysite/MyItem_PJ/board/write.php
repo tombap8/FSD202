ca1b48150d73456079c710afa2a99085ea8eb29d
Tom <tombap8@hotmail.com>
2021-03-24 14:48:26 +0900

    Create write.php

---
+++ 013.PHP학습/mysite/MyItem_PJ/board/write.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>게시판 - 글쓰기 페이지</title>
+    <link rel="stylesheet" href="board.css">
+</head>
+<body>
+    
+</body>
+</html>
