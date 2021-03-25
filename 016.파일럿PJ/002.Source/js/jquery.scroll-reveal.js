91d62801dae9e4480d9fc4c78ae68df856f8df2b
Tom <tombap8@hotmail.com>
2021-03-25 11:31:34 +0900

    aa

--- 016.파일럿PJ/002.Source/js/jquery.scroll-reveal.js
+++ 016.파일럿PJ/002.Source/js/jquery.scroll-reveal.js
@@ -7,12 +7,21 @@
             windowWidth = $(window).width(),
             initialScroll = $(window).scrollTop(),
             items = $('.js-reveal'),
+            items2 = $('.js-reveal2'), //확장샘플
             scroll;
 
         //hide anything not in the viewport
         items.each(function(){
             if($(this).offset().top >= windowHeight){
                 $(this).addClass('js-reveal--hidden');
+            }
+        });
+        //hide anything not in the viewport
+        
+        //확장샘플
+        items2.each(function(){
+            if($(this).offset().top >= windowHeight){
+                $(this).addClass('js-reveal2--hidden');
             }
         });
 
@@ -24,6 +33,12 @@
             items.each( function(){
                 if($(this).offset().top <= (scroll + windowHeight)){
                     $(this).removeClass('js-reveal--hidden');
+                }
+            });
+            // 확장샘플
+            items2.each( function(){
+                if($(this).offset().top <= (scroll + windowHeight)){
+                    $(this).removeClass('js-reveal2--hidden');
                 }
             });
         });
